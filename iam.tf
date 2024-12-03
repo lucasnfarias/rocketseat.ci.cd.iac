19b19d08922efe8ebf1575c831b951d697b1c54d
Lucas Farias <lucas.farias@rtbhouse.com>
2024-12-03 10:35:31 -0300

    add tf inline policy

--- iam.tf
+++ iam.tf
@@ -41,6 +41,29 @@ resource "aws_iam_role" "tf-role" {
   }
 }
 
+resource "aws_iam_role_policy" "tf-role-permissions" {
+  name = "tf-role-permissions"
+  role = aws_iam_role.tf-role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "Statement1"
+        Action   = "ecr:*"
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Sid      = "Statement2"
+        Action   = "iam:*"
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role" "app-runner-role" {
   name = "app-runner-role"
 
