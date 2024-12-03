667577afc11e1198387482bcc05ddab7cdcdc5dc
Lucas Farias <lucas.farias@rtbhouse.com>
2024-12-03 10:51:49 -0300

    add s3 state backend

--- main.tf
+++ main.tf
@@ -5,8 +5,36 @@ terraform {
       version = "5.78.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "rocketseat-ci-cd-iac"
+    key     = "state/terraform.tfstate"
+    region  = "us-east-2"
+  }
 }
 
 provider "aws" {
   region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "rocketseat-ci-cd-iac"
+
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = "rocketseat-ci-cd-iac"
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
